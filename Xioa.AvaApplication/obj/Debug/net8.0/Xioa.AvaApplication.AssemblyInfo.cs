baf1379405064502add126f8142d69c0d7bf1465
Xioa <132796255@qq.com>
2024-12-24 16:17:30 +0800

    init

--- Xioa.AvaApplication/obj/Debug/net8.0/Xioa.AvaApplication.AssemblyInfo.cs
+++ Xioa.AvaApplication/obj/Debug/net8.0/Xioa.AvaApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xioa.AvaApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09f6d3c63019f9dba1d678011a30cf27bd3a989")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xioa.AvaApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xioa.AvaApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
